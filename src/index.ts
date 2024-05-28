61515ca4cb17e75fd241882010d8b0f37d1ea78c
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-05-28 12:42:06 +0200

    Add Message class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,86 @@
 const inquirer = require('inquirer');
+const { consola, createConsola } = require("consola");
 
-inquirer.prompt([{
-  name: 'name',
-  type: 'input',
-  message: 'What\'s your name?',
-}, {
-  name: 'age',
-  type: 'number',
-  message: 'How old are you?',
-  default: 18,
-}]).then((answers: { name: string, age: number}) => {
-  console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
-});
+enum Action {
+  List = "list",
+  Add = "add",
+  Remove = "remove",
+  Quit = "quit"
+}
+
+type InquirerAnswers = {
+  action: Action
+}
+
+enum MessageVariant {
+  Success,
+  Error,
+  Info
+}
+
+class Message {
+  private content: string;
+
+  constructor(content: string) {
+    this.content = content;
+  }
+
+  public show(): void {
+    console.log(this.content);
+  }
+
+  public capitalize(): void {
+    this.content = this.content.charAt(0).toUpperCase() + this.content.slice(1).toLowerCase();
+  }
+
+  public toUpperCase(): void {
+    this.content = this.content.toUpperCase();
+  }
+
+  public toLowerCase(): void {
+    this.content = this.content.toLowerCase();
+  }
+
+  static showColorized( variant:MessageVariant , str: string):void {
+    
+    if (variant === MessageVariant.Success) consola.success(str);
+    else if(variant === MessageVariant.Error) consola.error(str);
+    else if (variant === MessageVariant.Info) consola.info(str);
+  }
+}
+
+const startApp = () => {
+  inquirer.prompt([{
+    name: 'action',
+    type: 'input',
+    message: 'How can I help you?',
+  }]).then(async (answers: InquirerAnswers) => {
+    switch (answers.action){
+      case Action.List:
+        console.log(answers.action);
+        break;
+      case Action.Add:
+        console.log(answers.action);
+        break;
+      case Action.Remove:
+        console.log(answers.action);
+        break;
+      case Action.Quit:
+        console.log(answers.action);
+        return;
+    }
+    startApp();
+  });
+}
+
+const msg = new Message("heLlo world!");
+msg.show(); // "heLlo world!"
+msg.capitalize();
+msg.show(); // "Hello world!"
+msg.toLowerCase();
+msg.show(); // "hello world!"
+msg.toUpperCase();
+msg.show(); // "HELLO WORLD!"
+Message.showColorized(MessageVariant.Success, "Test"); // √ "Test"
+Message.showColorized(MessageVariant.Error, "Test 2"); // "x Test 2"
+Message.showColorized(MessageVariant.Info, "Test 3"); // ℹ "Test 3"
